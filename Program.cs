52839bf7b5ae8f9550d58a70a0da1c4608f3627c
Ben Coveney <bencoveney@gmail.com>
2018-07-21 14:35:31 +0100

    initialised project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace esolangs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
